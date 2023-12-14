8a9a60fb0faa3c949f72dc782b0b83193a825f9d
Ken Lauer <ken@xlight.com>
2023-12-14 09:36:50 -0800

    WIP: update cmake platform flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,12 @@
+#!/usr/bin/env bash
+
 set -ex
 
-if [[ $(uname) == "Darwin" ]] ; then
+if [[ "$target_platform" == osx* ]]; then
+    CMAKE_PLATFORM_FLAGS+=" -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}"
+    CMAKE_PLATFORM_FLAGS+=" -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}"
     # disable xcairo device on macOS, since conda-forge's cairo doesn't support X11
+    # ref: https://github.com/conda-forge/cairo-feedstock/issues/22
     CMAKE_ARGS="${CMAKE_ARGS} -DPLD_xcairo=OFF "
 fi
 
