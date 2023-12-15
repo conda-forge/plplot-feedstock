c9ea4a1b1646d90b90fcc446c57650b0b58cfe0f
Ken Lauer <ken@xlight.com>
2023-12-15 09:46:07 -0800

    WIP: add back native build

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,10 +4,37 @@ set -ex
 
 if [[ "$target_platform" == osx* ]]; then
     CMAKE_PLATFORM_FLAGS+=" -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT}"
-    CMAKE_PLATFORM_FLAGS+=" -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}"
     # disable xcairo device on macOS, since conda-forge's cairo doesn't support X11
     # ref: https://github.com/conda-forge/cairo-feedstock/issues/22
     CMAKE_ARGS="${CMAKE_ARGS} -DPLD_xcairo=OFF "
+
+    if [[ "${CONDA_BUILD_CROSS_COMPILATION}" == "1" ]]; then
+       _MACOSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}
+       unset MACOSX_DEPLOYMENT_TARGET
+       cmake \
+           -DCMAKE_C_COMPILER="${CC_FOR_BUILD}" \
+           -DCMAKE_CXX_COMPILER="${CXX_FOR_BUILD}" \
+           -DCMAKE_Fortran_COMPILER="${FC_FOR_BUILD}" \
+           -DCMAKE_C_FLAGS="" \
+           -DCMAKE_CXX_FLAGS="" \
+           -DCMAKE_Fortran_FLAGS="" \
+           -DCMAKE_EXE_LINKER_FLAGS="" \
+           -DDEFAULT_NO_DEVICES=ON \
+           -DDEFAULT_NO_BINDINGS=ON \
+           -DPKG_CONFIG_EXECUTABLE=Not-Found \
+           -DENABLE_octave=OFF \
+           -DENABLE_tcl=ON \
+           -DENABLE_tk=ON \
+           -DPLD_xcairo=OFF \
+           -DBUILD_TEST=ON \
+           -B_build_native \
+           -GNinja
+        cmake --build _build_native
+        CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_NATIVE_BINARY_DIR=$PWD/_build_native"
+        CMAKE_ARGS="${CMAKE_ARGS} -DNaNAwareCCompiler=ON"
+        export MACOSX_DEPLOYMENT_TARGET=${_MACOSX_DEPLOYMENT_TARGET}
+    fi
+    CMAKE_PLATFORM_FLAGS+=" -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}"
 fi
 
 # since plplot overrides the CMAKE_INSTALL_* variables, we need to cleanup what is set by conda
